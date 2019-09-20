a9b991d654637363623186ff92fa2cf0ca2bee82
Bradshaw <Tony.Bradshaw@cmc.com>
2019-09-20 16:20:29 -0500

    First push

---
+++ scripts.js
@@ -0,0 +1,24 @@
+(function() {
+/*	const parentNav = document.querySelector('.parent-nav');
+	const dropdown = document.querySelector('.dropdown');
+
+	parentNav.addEventListener('mouseenter', function(e) {
+		menuTransition();
+		this.classList.add('dropdown-open');
+	});
+
+	parentNav.addEventListener('mouseleave', function(e) {
+		menuTransition();
+		console.log(e.target);
+		this.classList.remove('dropdown-open');
+	});
+
+	function menuTransition() {
+		const dropDownContent = document.querySelector('.dropdown-content');
+		dropDownContent.classList.add('dropdown-transition');
+
+		setTimeout(function() {
+			dropDownContent.classList.remove('dropdown-transition');
+		}, 500);
+	}*/
+})();
